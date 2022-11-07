89361a8f7de8822c6ae3f7c6528086786026e301
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-07 18:44:43 +0200

    Introduced JPA support and introduced all content-related entities.

--- src/main/java/com/acmeflix/domain/Movie.java
+++ src/main/java/com/acmeflix/domain/Movie.java
@@ -1,14 +1,26 @@
 package com.acmeflix.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
 @SuperBuilder
+@NoArgsConstructor
+@Entity
+@Table(name = "MOVIES", indexes = {@Index(columnList = "duration_in_minutes")})
+@SequenceGenerator(name = "idGenerator", sequenceName = "MOVIES_SEQ", initialValue = 1, allocationSize = 1)
 public class Movie extends Content {
+	@Column(name = "duration_in_minutes", nullable = false)
 	private Integer durationInMinutes;
 }
--- src/main/java/com/acmeflix/domain/TvShow.java
+++ src/main/java/com/acmeflix/domain/TvShow.java
@@ -1,10 +1,14 @@
 package com.acmeflix.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +16,10 @@ import java.util.Set;
 @Setter
 @ToString(callSuper = true)
 @SuperBuilder
+@NoArgsConstructor
+@Entity
+@Table(name = "TV_SHOWS")
+@SequenceGenerator(name = "idGenerator", sequenceName = "TV_SHOWS_SEQ", initialValue = 1, allocationSize = 1)
 public class TvShow extends Content {
 	private Set<Season> seasons = new HashSet<>();
 }
