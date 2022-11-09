2876d5528b8846b1c2d8601024c15939d1f91611
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-09 15:53:54 +0200

    Added an enumeration for the specific subscriptions plans we intend to use.

---
+++ src/main/java/com/acmeflix/domain/SubscriptionPlans.java
@@ -0,0 +1,27 @@
+package com.acmeflix.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+public enum SubscriptionPlans {
+	//@formatter:off
+	BASIC(SubscriptionPlan.builder()
+						  .title("Basic")
+						  .description("Good video quality in SD (480p).")
+						  .costPerMonth(BigDecimal.valueOf(7.99)).build()),
+	STANDARD(SubscriptionPlan.builder()
+							 .title("Standard")
+							 .description("Great video quality in Full HD (1080p).")
+							 .costPerMonth(BigDecimal.valueOf(10.99)).build()),
+	PREMIUM(SubscriptionPlan.builder()
+							.title("Premium")
+							.description("Best video quality in Ultra HD (4K) and HDR.")
+							.costPerMonth(BigDecimal.valueOf(13.99)).build());
+
+	//@formatter:on
+	private final SubscriptionPlan subscriptionPlan;
+}
