b6ba17bd6877b6e4723bc2d322243a76577df978
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-09 12:39:39 +0200

    Added a custom exception to be used while checking age limits during rating.

---
+++ src/main/java/com/acmeflix/exception/ViewingRestrictionException.java
@@ -0,0 +1,7 @@
+package com.acmeflix.exception;
+
+public class ViewingRestrictionException extends Exception {
+	public ViewingRestrictionException(final String message) {
+		super(message);
+	}
+}
