9343491fd41ebf821d655b2dea09bdfc9e943ef3
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-09 15:56:53 +0200

    Added a bootstrap runner class to insert and update data following different scenarios and approaches.

---
+++ src/main/java/com/acmeflix/bootstrap/SampleContentCreator.java
@@ -0,0 +1,206 @@
+package com.acmeflix.bootstrap;
+
+import com.acmeflix.base.BaseComponent;
+import com.acmeflix.domain.*;
+import com.acmeflix.service.AccountService;
+import com.acmeflix.service.CastMemberService;
+import com.acmeflix.service.MovieService;
+import com.acmeflix.service.PersonService;
+import com.acmeflix.service.SubscriptionPlanService;
+import com.acmeflix.service.TvShowService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+//@Profile("generate-sample-data")
+@AllArgsConstructor
+public class SampleContentCreator extends BaseComponent implements CommandLineRunner {
+	private final AccountService accountService;
+	private final CastMemberService castMemberService;
+	private final MovieService movieService;
+	private final PersonService personService;
+	private final SubscriptionPlanService subscriptionPlanService;
+	private final TvShowService tvShowService;
+
+	@Override
+	public void run(final String... args) throws Exception {
+		subscriptionPlanService.createAll(SubscriptionPlans.BASIC.getSubscriptionPlan(),
+										  SubscriptionPlans.STANDARD.getSubscriptionPlan(),
+										  SubscriptionPlans.PREMIUM.getSubscriptionPlan());
+
+		//@formatter:off
+		var firstAccount = Account.builder()
+								  .email("c.giannacoulis@codehub.g")
+								  .password("test123")
+								  .subscriptionPlan(subscriptionPlanService.getByTitle("STANDARD")).build();
+
+		var firstCreditCard = CreditCard.builder()
+										.cardNumber("1234432156788765")
+										.cardHolder("XXX")
+										.expirationDate("01/27")
+										.securityCode(123).build();
+
+		accountService.addCreditCard(firstAccount, firstCreditCard);
+		firstAccount.setProfiles(Set.of(com.acmeflix.domain.Profile.builder()
+																   .name("adult1")
+																   .language(Language.ENGLISH)
+																   .viewingRestriction(ViewingRestriction.ALL)
+																   .account(firstAccount)
+																   .build(),
+										com.acmeflix.domain.Profile.builder()
+																   .name("adult2")
+																   .language(Language.ENGLISH)
+																   .viewingRestriction(ViewingRestriction.EIGHTEEN)
+																   .account(firstAccount)
+																   .build()));
+
+		var firstAccountSaved = accountService.create(firstAccount);
+		logger.info("Account saved: {}.", firstAccountSaved);
+
+		var firstAccountRetrieved = accountService.get(firstAccountSaved.getId());
+		logger.info("Account retrieved: {}.", firstAccountRetrieved);
+
+		var firstAccountFullyRetrieved = accountService.getFullContent(firstAccountSaved.getId());
+		logger.info("Account retrieved: {}.", firstAccountFullyRetrieved);
+
+		// Create first movie
+		Movie firstMovie = Movie.builder()
+								.title("Man From Toronto")
+								.plot("The world's deadliest assassin and New York's biggest screw-up are mistaken for each other at an Airbnb rental.")
+								.releaseYear(2022)
+								.durationInMinutes(112)
+								.genres(Set.of(Genre.ACTION, Genre.COMEDY))
+								.viewingRestriction(ViewingRestriction.THIRTEEN)
+								.audioLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								.subtitleLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								.build();
+		// Create second movie
+		Movie secondMovie = Movie.builder()
+								 .title("Red Notice")
+								 .plot("An Interpol agent successfully tracks down the world's most wanted art thief, with help from a rival thief. But nothing is as it seems, as a series of double-crosses ensue.")
+								 .releaseYear(2022)
+								 .durationInMinutes(118)
+								 .genres(Set.of(Genre.ACTION, Genre.COMEDY))
+								 .viewingRestriction(ViewingRestriction.THIRTEEN)
+								 .audioLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								 .subtitleLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								 .build();
+
+		// Set second movie as a recommendation to the first one
+		firstMovie.setRecommendations(Set.of(secondMovie));
+
+		movieService.createAll(firstMovie, secondMovie);
+
+		// First TV Show
+		TvShow firstTvShow = TvShow.builder()
+								   .title("Stranger Things")
+								   .plot("When a young boy disappears, his mother, a police chief and his friends must confront terrifying supernatural forces in order to get him back.")
+								   .releaseYear(2016)
+								   .genres(Set.of(Genre.ACTION, Genre.COMEDY))
+								   .viewingRestriction(ViewingRestriction.THIRTEEN)
+								   .audioLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								   .subtitleLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								   .seasons(Set.of(
+										   Season.builder()
+												 .title("Season 1")
+												 .ordering(1)
+												 .releaseYear(2016)
+												 .episodes(Set.of(
+														 Episode.builder().title("Chapter One: The Vanishing of Will Byers").durationInMinutes(49).build(),
+														 Episode.builder().title("Chapter Two: The Weirdo on Maple Street").durationInMinutes(56).build(),
+														 Episode.builder().title("Chapter Three: Holly, Jolly").durationInMinutes(52).build(),
+														 Episode.builder().title("Chapter Four: The Body").durationInMinutes(51).build()))
+												 .build(),
+										   Season.builder()
+												 .title("Season 2")
+												 .ordering(2)
+												 .releaseYear(2017)
+												 .episodes(Set.of(
+														 Episode.builder().title("Chapter One: MADMAX").durationInMinutes(48).build(),
+														 Episode.builder().title("Chapter Two: Trick or Treat, Freak").durationInMinutes(56).build(),
+														 Episode.builder().title("Chapter Three: The Pollywog").durationInMinutes(51).build(),
+														 Episode.builder().title("Chapter Four: Will the Wise").durationInMinutes(46).build()))
+												 .build(),
+										   Season.builder()
+												 .title("Season 3")
+												 .ordering(3)
+												 .releaseYear(2019)
+												 .episodes(Set.of(
+														 Episode.builder().title("Chapter One: Suzie, Do You Copy?").durationInMinutes(51).build(),
+														 Episode.builder().title("Chapter Two: The Mall Rats").durationInMinutes(50).build(),
+														 Episode.builder().title("Chapter Three: The Case of the Missing Lifeguard").durationInMinutes(50).build(),
+														 Episode.builder().title("Chapter Four: The Sauna Test").durationInMinutes(53).build()))
+												 .build(),
+										   Season.builder()
+												 .title("Season 4")
+												 .ordering(4)
+												 .releaseYear(2022)
+												 .episodes(Set.of(
+														 Episode.builder().title("Chapter One: The Hellfire Club").durationInMinutes(78).build(),
+														 Episode.builder().title("Chapter Two: Vecna's Curse").durationInMinutes(77).build(),
+														 Episode.builder().title("Chapter Three: The Monster and the Superhero").durationInMinutes(64).build(),
+														 Episode.builder().title("Chapter Four: Dear Billy").durationInMinutes(79).build()))
+												 .build()))
+								   .build();
+		// setting TV Show for each season
+		firstTvShow.getSeasons().forEach(season -> season.setTvShow(firstTvShow));
+
+		// setting season for each episode
+		firstTvShow.getSeasons().forEach(season -> season.getEpisodes().forEach(episode -> episode.setSeason(season)));
+
+		tvShowService.create(firstTvShow);
+
+		// Using services now
+		var secondTvShow = TvShow.builder()
+								 .title("Peaky Blinders")
+								 .plot("A gangster family epic set in 1900s England, centering on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby.")
+								 .releaseYear(2013)
+								 .genres(Set.of(Genre.ACTION, Genre.DRAMA))
+								 .viewingRestriction(ViewingRestriction.THIRTEEN)
+								 .audioLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								 .subtitleLanguages(Set.of(Language.ENGLISH, Language.GREEK))
+								 .build();
+
+		tvShowService.addSeasons(secondTvShow,
+								 Season.builder().title("Season 1").ordering(1).releaseYear(2013).build(),
+								 Season.builder().title("Season 2").ordering(2).releaseYear(2014).build(),
+								 Season.builder().title("Season 3").ordering(3).releaseYear(2016).build(),
+								 Season.builder().title("Season 4").ordering(4).releaseYear(2017).build(),
+								 Season.builder().title("Season 5").ordering(5).releaseYear(2019).build(),
+								 Season.builder().title("Season 6").ordering(6).releaseYear(2022).build());
+
+		tvShowService.create(secondTvShow);
+
+		var secondTvShowRetrieved = tvShowService.getFullContent(4L);
+
+		tvShowService.addEpisodes(tvShowService.getSeason(secondTvShowRetrieved, 5),
+								  Episode.builder().title("Black Day").durationInMinutes(55).build(),
+								  Episode.builder().title("Black Shirt").durationInMinutes(59).build(),
+								  Episode.builder().title("Gold").durationInMinutes(56).build(),
+								  Episode.builder().title("Sapphire").durationInMinutes(59).build(),
+								  Episode.builder().title("The Road to Hell").durationInMinutes(59).build(),
+								  Episode.builder().title("Lock and Key").durationInMinutes(82).build());
+
+		tvShowService.update(secondTvShowRetrieved);
+
+		personService.createAll(Person.builder().firstName("Tom").lastName("Cruise").build(),
+								Person.builder().firstName("Johnny").lastName("Depp").build());
+
+		var tomCruise = personService.findByLastNameAndFirstName("Cruise", "Tom");
+		var johnnyDepp = personService.findByLastNameAndFirstName("Depp", "Johnny");
+
+		castMemberService.create(CastMember.builder()
+										   .person(tomCruise)
+										   .content(secondTvShowRetrieved).role(Role.ACTOR).build());
+		castMemberService.create(CastMember.builder()
+										   .person(johnnyDepp)
+										   .content(secondTvShowRetrieved)
+										   .role(Role.ACTOR)
+										   .build());
+
+		//@formatter:on
+	}
+}
